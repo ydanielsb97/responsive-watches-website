5055b6056cdfb0a17b9ec77d2dea9674791cb1ab
Yeison Serrano <ydanielsb97@gmail.com>
2023-11-30 09:16:08 -0400

    ✨ Cleaning up the comments and spaces

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,33 +1,26 @@
-/*=============== SHOW MENU ===============*/
-
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-/* Menu show */
 if (navToggle)
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 
-/* Menu hidden */
 if (navClose)
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 
-/*=============== REMOVE MENU MOBILE ===============*/
 
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
-    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*=============== SWIPER WACTHES ===============*/
 
 const swiperWatches = new Swiper('.home__swiper', {
     loop: true,
@@ -52,7 +45,6 @@ const swiperWatches = new Swiper('.home__swiper', {
     },
   });
 
-/*=============== GSAP ANIMATION ===============*/
 
 gsap.from('.home__images', 1.5, { opacity: 0, y: 150, delay: .1})
 gsap.from('.home__data', 1.8, { opacity: 0, y: -100, delay: .8})
